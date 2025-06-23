781f52035d679a8e1255105813cbb75bda98b84c
ReythemGespenst <roncajumban@gmail.com>
2025-06-23 15:56:36 +0800

    Added ejs to packages, index.js modified to include ejs view engine

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get('/', (req,res,next) => {
     console.log('test')
-    res.render("Hi")
+    res.render("login")
 })
 
 app.listen(3000)
+console.log("Listening at port 3000")
