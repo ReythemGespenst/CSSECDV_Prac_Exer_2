12be4f7d3993b69c9d4c8f81ce80f53300ba677a
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2025-06-25 17:01:14 +0800

    feat!: improvements in the codes (read description)
    -postRouter.js
    -- added the backend logic with password hashing using bcrypt in register
    -- suggested fix for login logic
    
    -register.ejs
    -- fixed the register page by adding name and id in every input except the email part since i commented this out for now
    
    -index.js
    -- added the db string connections here
    
    -user.js
    -- fixed the user model
    
    needed fixes:
    - put the db connection string in .env file
    - don't forget to remove the test IP address in mongodb atlas since it is accessible by all of IP addresses
    - uncomment the equire('dotenv').config() after transferring the db credentials in .env file

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 const express = require('express')
 const session = require('express-session')
 const mongoose = require('mongoose');
-
+const collection = require("./models/user");
 
 const app = express() 
 const MongoStore = require('connect-mongo')
 
 // connect to db atlas
-const dbURI = 'mongodb+srv://group2:MrsA0tzKYszunkr3@pe2.g9bjw1d.mongodb.net/?retryWrites=true&w=majority&appName=PE2';
+// const dbURI = 'mongodb+srv://group2:MrsA0tzKYszunkr3@pe2.g9bjw1d.mongodb.net/?retryWrites=true&w=majority&appName=PE2';
+const dbURI = 'mongodb+srv://group2:MrsA0tzKYszunkr3@pe2.g9bjw1d.mongodb.net/PE2?retryWrites=true&w=majority'
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log('connected to db'))
     .catch((err) => console.log(err));
 
 const getRouters = require('./routes/getRouter')
 const postRouters = require('./routes/postRouter');
-const { default: mongoose } = require('mongoose');
+// const { default: mongoose } = require('mongoose');
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: true}))
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,17 +1,69 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const collection = require("../models/user");
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req,res) => {
+
+    console.log("Headers:", req.headers['content-type']);
+    console.log("Raw Body:", req.body);
+    const {username, password, confirmPass} = req.body;
+
+    // assuming user must be unique.
+    // if user exist, redirect to login page
+    // res.redirect('login');
+    try{
+        // assuming we need to check if there's existing user...
+        // const existingUser = await collection.findOne({username});
+        // if(existingUser){
+        //     console.log("User already registered");
+        // }
+        console.log("Registering:", req.body);
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err) throw err;
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if(err) throw err;
+                try{
+                    console.log(collection);
+                    const newUser = await collection.create({
+                        username,
+                        password: hash
+                    });
+                    console.log("you are now registered. login now!");
+                    res.redirect('/login');
+                } catch(error){
+                    console.error(error);
+                    console.log('could not create user. Please try again');
+                    res.redirect('/register');
+                }
+            });
+        });
+    } catch(err){
+        console.error(error);
+        console.log('could not create user. Please try again');
+        res.redirect('/register');
+    }
 
 })
 
 router.post('/login', (req, res) =>{
-    const {username, password} = req.body;
+    try {
+        // const user = collection.collection({username: req.body.username, password: req.body.password});
+        const { username, password } = req.body;
+        console.log(user);
+        if (user) {  
+            console.log('logged in successfully');
+            res.redirect("/"); // this part will redirect the user to the main page
+        } else {
+            console.log('did not login successfully');
+            res.render("login", { title: "Login Account", error: "Invalid username or password" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal Server Error");
+		
 
-    console.log('Username: ', username)
-    console.log('Password: ', password)
-
-    res.redirect('/dashboard')
+    }
 })
 
 router.post('/signout', (req,res) => {
