d1f040f1fb38ae9637925e0f8be54cf092549ebc
ReythemGespenst <roncajumban@gmail.com>
2025-06-26 14:11:07 +0800

    updated db.js, index.js to include dotenv, getRouters, post router, and register
    db.js and index.js now accommodates dotenv where the mongo db uri is placed.
    
    username checking is finished at postrouter.
    
    register.ejs now shows errors if it exists

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// require('dotenv').config()
+require('dotenv').config()
+require('./db')
 
 const express = require('express')
 const session = require('express-session')
@@ -7,13 +8,6 @@ const collection = require("./models/user");
 
 const app = express() 
 const MongoStore = require('connect-mongo')
-
-// connect to db atlas
-// const dbURI = 'mongodb+srv://group2:MrsA0tzKYszunkr3@pe2.g9bjw1d.mongodb.net/?retryWrites=true&w=majority&appName=PE2';
-const dbURI = 'mongodb+srv://group2:MrsA0tzKYszunkr3@pe2.g9bjw1d.mongodb.net/PE2?retryWrites=true&w=majority'
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then((result) => console.log('connected to db'))
-    .catch((err) => console.log(err));
 
 const getRouters = require('./routes/getRouter')
 const postRouters = require('./routes/postRouter');
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,23 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const collection = require("../models/user");
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
-router.post('/register', (req,res) => {
+const usernameBlackList = [
+    'guys, help me out here'
+]
+
+router.post('/register', async (req,res) => {
 
     console.log("Headers:", req.headers['content-type']);
     console.log("Raw Body:", req.body);
     const {username, email, password, confirmPass} = req.body;
+    const display_name = username
 
     // assuming user must be unique.
     // if user exist, redirect to login page
-    // res.redirect('login');
+
+    //username checking
+    if (password != confirmPass) {
+        return res.render('register', {error: "Passwords do not match"});
+    }
+    if (username.length <= 2  || username.length >= 30) {
+        return res.render('register', {error: "Error: Username must be 3-30 characters long"})
+    }
+    if ((/^[a-zA-z0-9_-]+&/.test(username))){
+        return res.render('register', {error: "Error: Username can only contain letters, numbers, hyphens, and underscores"})
+    }
+    if (/[_-]{2,}/.test(username)){
+        return res.render('register', {error: "Error: Username cannot have two or more consecutive special characters"})
+    }
+    if(/^[_-]|[_-]$/.test(username)){
+        return res.render('register', {error: "Error: Username cannot start or end with special characters"})
+    }
+
+
     try{
         // assuming we need to check if there's existing user...
-        // const existingUser = await collection.findOne({username});
-        // if(existingUser){
-        //     console.log("User already registered");
-        // }
+        const lowerUser = username.toLowerCase()
+        const existingUser = await collection.findOne({username: lowerUser});
+        if(existingUser){
+            console.log("User already registered");
+            return res.render('register', {error: "Error: Username already exists"})
+        }
+
         console.log("Registering:", req.body);
         bcrypt.genSalt(10, (err, salt) => {
             if(err) throw err;
@@ -27,14 +53,17 @@ router.post('/register', (req,res) => {
                     console.log(collection);
                     const newUser = await collection.create({
                         username,
-                        password: hash
+                        display_name,
+                        email,
+                        password_hash: hash,
+                        hash_algorithm: 'bcrypt'
                     });
                     console.log("you are now registered. login now!");
-                    res.redirect('/login');
+                    res.redirect('/');
                 } catch(error){
                     console.error(error);
                     console.log('could not create user. Please try again');
-                    res.redirect('/register');
+                    res.redirect('/');
                 }
             });
         });
