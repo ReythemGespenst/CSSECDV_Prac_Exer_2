4fb0be89022f8d13f5f1dc12b2347c7e5dad2d85
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2025-07-19 20:52:38 +0800

    feat: added profile logic

--- routes/getRouter.js
+++ routes/getRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {isUserLoggedIn} = require('./util')
+const collection = require("../models/user");
 
 router.get('/', (req, res) => {
     res.redirect("/login")
@@ -26,6 +27,35 @@ router.get('/dashboard', (req,res) => {
     }
    
     res.render("dashboard", {username })
-})
+});
+
+router.get('/profile/:id', async (req,res) => {
+    const username = req.cookies.username;
+    if (!isUserLoggedIn(req)) {
+        return res.redirect('/login');
+    }
+    console.log("user details: ", req.username);
+    try {
+        const user = await collection.findId(req.user._id);
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+
+        res.render('profile', { username, email });
+    } catch (err) {
+        console.error("Error fetching user profile:", err);
+        res.status(500).send("Failed to load user profile");
+    }
+});
+
+router.get('/profile/edit', (req, res) => {
+	try {
+		const user = req.user;
+		res.render('editProfile', { user });
+	} catch (err) {
+		console.error(err);
+		res.status(500).send('Error loading profile');
+	}
+});
 
 module.exports = router
