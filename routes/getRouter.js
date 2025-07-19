b10888e39f0f0852b5d036e5c743d5cdc6301de5
Sanzai-X <130345226+Sanzai-X@users.noreply.github.com>
2025-07-19 22:20:18 +0800

    fix!: fix the logic on rendering the user's profile

--- routes/getRouter.js
+++ routes/getRouter.js
@@ -44,21 +44,23 @@ router.get('/profile', async (req,res) => {
             return res.status(404).send("User not found");
         }
 
-        res.render('profile', { username, email });
+        res.render('profile', { user: {username, email}, editable: false });
     } catch (err) {
         console.error("Error fetching user profile:", err);
         res.status(500).send("Failed to load user profile");
     }
 });
 
-router.get('/profile/edit', (req, res) => {
+router.get('/profile/edit', async (req, res) => {
 	try {
-		const user = req.user;
-		res.render('editProfile', { user });
+		const username = req.cookies.username;
+		const user = await collection.findOne({ username });
+		res.render('editProfile', { user, editable: true });
 	} catch (err) {
 		console.error(err);
 		res.status(500).send('Error loading profile');
 	}
 });
+
 
 module.exports = router
