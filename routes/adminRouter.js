7a8c318fffaea89795699e601677ef603c79eab7
Milkz03 <mikkel_gamboa@dlsu.edu.ph>
2025-07-19 22:02:55 +0800

    feat: list all users in /users

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -36,6 +36,7 @@ router.get('/users', async (req, res) => {
 	try {
 		// Fetch the user's role from the database
 		const user = await User.findOne({ display_name: username });
+		const users = await User.find();
 
 		if (!user) {
 			return res.render('dashboard', { username, error: 'User not found' });
@@ -43,24 +44,11 @@ router.get('/users', async (req, res) => {
 
 		const roles = await getUserRoles(user._id);
 
-		if (!roles.includes('admin') && !roles.includes('manager')) {
+		if (!(roles.includes('admin') || roles.includes('manager'))){
 			return res.render('dashboard', { username, roles, error: 'Access denied: Insufficient permissions' });
 		}
 
-// ! in case above code does not work 
-		// Check if the role is "admin" or "manager"
-		// const userRoleData = await UserRole.findOne({ user: user._id }).populate({
-		// 	path: 'role',
-		// 	model: 'Roles' // Ensure the correct model name is used
-		// });
-
-		// if (!userRoleData || (userRoleData.role.name !== 'admin' && userRoleData.role.name !== 'manager')) {
-		// 	return res.render('dashboard', { username, roles, error: 'Access denied: Insufficient permissions' });
-		// }
-		
-
-		// Render the users page if the role is valid
-		res.render('users', { username });
+		res.render('users', { username, users });
 	} catch (err) {
 		console.error(err);
 		res.render('dashboard', { username, roles, error: 'An error occurred. Please try again.' });
